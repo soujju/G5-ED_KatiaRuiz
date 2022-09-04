6b7a2419b344c659f037ee6b13b952ecaa0c7201
soujju <katia.ruiz@cetys.edu.mx>
2022-09-04 15:18:41 -0700

    Primeros pasos con c#
    tarea 1 creo

---
+++ Primeros pasos con c#/escritura.cs
@@ -0,0 +1,7 @@
+//Console.WriteLine("Hello World");
+
+Console.WriteLine("This is the first line");
+
+//Desafio
+Console.Write("This is");
+Console.Write("the second line");
---
+++ Primeros pasos con c#/operaciones.cs
@@ -0,0 +1,34 @@
+//Practica
+
+int x = 74;
+int y = 24;
+Console.WriteLine(x + y);
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+
+int value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine("Second: " + value++);
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
+
+//Desafio
+
+int fahrenheit = 94;
+double celsius;
+
+celsius = (fahrenheit - 32) * 0.5555;
+Console.WriteLine("The temperature is: " + celsius + " Celsius");
