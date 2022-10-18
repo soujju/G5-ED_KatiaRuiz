03850336b2d52d2a771621a88aac64ad7a1289a7
soujju <katia.ruiz@cetys.edu.mx>
2022-10-18 10:36:44 -0700

    calculadora examen2

---
+++ calculadora-estructura/calculadora-estructura/calculadora.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace calculadoraestructura
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double num=0;
+            double num2=0;
+            double res;
+            string opera;
+            string res2;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("ingresa primer numero:");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ingresa operacion (/,+,-,*,^):");
+                opera = Console.ReadLine();
+                Console.WriteLine("ingresa segundo numero:");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                switch (opera)
+                {
+                    case "+":
+                        res = (num + num2);
+                        Console.WriteLine("resultado: " + res);
+                        break;
+
+                    case "-":
+                        res = (num - num2);
+                        Console.WriteLine("resultado: " + res);
+                        break;
+
+                    case "x":
+                        res = (num * num2);
+                        Console.WriteLine("resultado: " + res);
+                        break;
+
+                    case "/":
+                        res = (num / num2);
+                        Console.WriteLine("resultado: " + res);
+                        break;
+                    case "^":
+                        res = Math.Pow(num, num2);
+                        Console.WriteLine("resultado: " + res);
+                        break;
+                }
+                Console.ReadLine();
+                Console.WriteLine("desea hacer otra operacion? (s/n)");
+                res2 = Console.ReadLine();
+
+            } while (res2 == "s");
+        }
+
+    }
+}
